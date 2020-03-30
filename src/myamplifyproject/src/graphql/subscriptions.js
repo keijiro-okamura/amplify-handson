f211b9893f023f5b22bdade92ac63348e4401ec1
keijiro-okamura <keijiro.okamura@excite.jp>
2020-03-30 15:32:25 +0900

    [add]handson実装

---
+++ src/myamplifyproject/src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      username
+      content
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      username
+      content
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      username
+      content
+    }
+  }
+`;
